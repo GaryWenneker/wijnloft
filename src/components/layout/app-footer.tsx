449635ce512431abd82d253f43d8e52d50dcc0de
Peter van Koesveld <peterkoesveld@gmail.com>
2021-01-11 15:49:58 +0100

    Updated spacings and typography for home and footer

--- src/components/layout/app-footer.tsx
+++ src/components/layout/app-footer.tsx
@@ -16,23 +16,23 @@ export class IpAppFooter {
                         <div class="twelve column">
                             <div class="row">
                                 <div class="four columns offset-by-two">
-                                    <div class="twelve columns label" style={{ marginTop: '80px' }}>Geen 18, Geen alcohol.</div>
+                                    <div class="twelve columns label-standard" style={{ marginTop: '80px' }}>Geen 18, Geen alcohol.</div>
                                     <div class="row">
                                         <div class="six columns" style={{ marginTop: '32px' }}>
-                                            <div class="twelve columns label">Klantenservice</div>
-                                            <div class="twelve columns label">Privacy</div>
-                                            <div class="twelve columns label">Algemene voorwaarden</div>
+                                            <div class="twelve columns label-standard">Klantenservice</div>
+                                            <div class="twelve columns label-standard">Privacy</div>
+                                            <div class="twelve columns label-standard">Algemene voorwaarden</div>
                                         </div>
                                         <div class="six columns" style={{ marginTop: '32px' }}>
-                                            <div class="twelve columns label">Klantenservice</div>
-                                            <div class="twelve columns label">Privacy</div>
-                                            <div class="twelve columns label">Algemene voorwaarden</div>
+                                            <div class="twelve columns label-standard">Klantenservice</div>
+                                            <div class="twelve columns label-standard">Privacy</div>
+                                            <div class="twelve columns label-standard">Algemene voorwaarden</div>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="four columns">
-                                    <div class="twelve columns label" style={{ marginTop: '80px' }}>Copyright © 2020 WijnLoft Alle rechten voorbehouden</div>
-                                    <div class="twelve columns label" style={{ marginTop: '32px' }}>Gratis verzending vanaf 12 flessen of 100 euro – Op werkdagen besteld voor 11 uur is morgen in huis</div>
+                                    <div class="twelve columns label-standard" style={{ marginTop: '80px' }}>Copyright © 2020 WijnLoft. Alle rechten voorbehouden</div>
+                                    <div class="twelve columns label-standard" style={{ marginTop: '32px' }}>Gratis verzending vanaf 12 flessen of 100 euro – Op werkdagen besteld voor 11 uur is morgen in huis</div>
                                 </div>
                             </div>
                         </div>
